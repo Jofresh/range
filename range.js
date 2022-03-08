909b744f6faa80f86903e28d210769c9e244aa80
Geoffray Gaborit <gaborit.geoffray@icloud.com>
2022-03-08 15:12:16 +0100

    commented

--- range.js
+++ range.js
@@ -7,12 +7,17 @@ function range(...args) {
     return createArray(start, stop, step, reverse)
 }
 
+// Construit le tableau
 function createArray(start, stop, step, reverse) {
     let length = getLength(start, stop, step)
-    let arr = new Array(length)
-
-    let direction = reverse ? -1 : 1
-    if (reverse) [start, stop] = [stop, start]
+    let arr = new Array(length) // Créé un tableau de la taille calculée
+    
+    // Si on veut le tableau inversé alors on commence par la fin, en allant de droite à gauche.
+    let direction = 1                   // start = start + step 
+    if (reverse) {
+        [start, stop] = [stop, start]   // On swap start et stop.
+        direction = -1                  // stop = stop - step
+    }
 
     let i = 0;
     while (i < length) {
@@ -23,6 +28,7 @@ function createArray(start, stop, step, reverse) {
     return arr
 }
 
+// Calcule la taille du tableau en fonction des paramètres.
 function getLength(start, stop, step) {
     return Math.abs(Math.ceil((stop - start) / step))
 }
