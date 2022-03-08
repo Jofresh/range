7129d6dc0dfd300fc4720792146613a853999cf7
Geoffray Gaborit <gaborit.geoffray@icloud.com>
2022-03-08 10:54:14 +0100

    init

--- range.js
+++ range.js
@@ -1,33 +1,45 @@
 function range(...args) {
-    if (args.length === 1) return Array.from({length: args[0]}, (x,i) => i)
-
-    // if (args[0] > args[1]) return 'First argument has to be lower than the first one.'
-
-    let start = args[0],
-        stop = args[1],
-        step = (args[2] || 1)
-        reverse = args[3]
+    let [start, stop = 0, step = 1, reverse] = args
     
-    return reverse ? getReversed(start, stop, step) : getArray(start, stop, step);
+    if (stop === 0 && start > 0) [start, stop] = [stop, start]
+
+    if (reverse)
+        return reversedArray(start, stop, step)
+    else
+        return invertArray(start, stop, step)
 }
 
-function getArray(start, stop, step) {
-    let length = getLength(start, stop, step);
-    return Array.from({length}, (item, index) => start + index * step)
+function invertArray(start, stop, step) {
+    let length = size(start, stop, step)
+    let arr = new Array(length)
+
+    let i = 0;
+    while (i < length) {
+        arr[i++] = start
+        start += step
+    }
+
+    return arr
 }
 
-function getReversed(start, stop, step) {
-    let length = getLength(start, stop, step);
-    return Array.from({length}, (item, index) => stop - (index + 1) * step)
+function reversedArray(start, stop, step) {
+    let length = size(start, stop, step)
+    let arr = new Array(length)
+    
+    let i = 0
+    stop -= step
+    while (i < length) {
+        arr[i++] = stop
+        stop -= step
+    }
+
+    return arr
 }
 
-function getLength(start, stop, step) {
-    return Math.ceil((stop - start) / step);
+function size(start, stop, step) {
+    return Math.ceil((stop - start) / step)
 }
 
 module.exports = {
     range
 }
-
-// module.exports.range = range
-// module.exports = range
